f512327c5a2da7e958b821b7f7f994f1f3224740
axius16 <naza16@gmail.com>
2014-06-16 00:47:53 -0300

    DBConnection
    Subo le primer version del DBConnection. Primero necesito lograr
    configurar la BD para poder probarlo :/ pero deberia funcionar OK

---
+++ FrbaCommerce/FrbaCommerce/Connection/DBConexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace FrbaCommerce
+{
+    public class DBConexion
+    {
+        public static SqlConnection obtenerConexion()
+        {
+            SqlConnection conn = new SqlConnection(@"Data source = localhost\SQLSERVER2008; Initial catalog = GD1C2014 ; User Id = gd; Password = gd2014 ");
+            conn.Open();
+            return conn;
+        }
+
+    }
+}
